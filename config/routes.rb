1531b58a3cc2e28165f6d6d05fb56a7df8279aed
Chris Schmitz <ccschmitz@gmail.com>
2016-06-06 16:41:31 -0500

    Stub intros for life accounts and eulogies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :eulogies, except: :index
+  resources :eulogies, except: :index do
+    collection do
+      get :intro
+    end
+  end
   resources :life_accounts, except: :index do
     resources :commitments, except: :index
+
+    collection do
+      get :intro
+    end
   end
 
   devise_for :users
